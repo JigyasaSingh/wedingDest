84d86fec3e2c6eb43e24c53453f2f66311b39e00
jigyasakumari <jigyasakumari3@gmail.com>
2017-07-16 09:01:15 +0530

    done with save team api

--- api/controllers/TeamController.js
+++ api/controllers/TeamController.js
@@ -1,5 +1,14 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
 var controller = {
-    
+    saveInTeam: function (req, res) {
+        if (req.body) {
+            Team.saveInTeam(req.body, res.callback);
+        }else{
+            res.json({
+                value: false,
+                data: "User Not logged in"
+            });
+        }
+    },
 };
 module.exports = _.assign(module.exports, controller);
--- api/services/Team.js
+++ api/services/Team.js
@@ -9,6 +9,7 @@ var objectid = require("mongodb").ObjectID;
 var moment = require('moment');
 var request = require("request");
 var schema = new Schema({
+autoID:Number,
 tagline:String,
 teamId:String,
 memberTeam: [{
@@ -16,17 +17,112 @@ memberTeam: [{
         ref: 'Member',
         index: true
     }],
+
     
 });
 
 schema.plugin(deepPopulate, {});
 schema.plugin(uniqueValidator);
 schema.plugin(timestamps);
+// schema.plugin(autoIncrement.plugin, {
+//     model: 'Team',
+//     field: 'autoID',
+//     startAt: 1,
+//     incrementBy: 1
+// });
 module.exports = mongoose.model('Team', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
 var model = {
 
+    search: function (data, callback) {
+        var Model = this;
+        var Const = this(data);
+        var maxRow = Config.maxRow;
+
+        var page = 1;
+        if (data.page) {
+            page = data.page;
+        }
+        var field = data.field;
+        var options = {
+            field: data.field,
+            filters: {
+                keyword: {
+                    fields: ['tagline'],
+                    term: data.keyword
+                }
+            },
+            sort: {
+                desc: 'createdAt'
+            },
+            start: (page - 1) * maxRow,
+            count: maxRow
+        };
+        
+            var deepSearch = " ";
+            var Search = Model.find(data.keyword)
+
+                .order(options)
+                // .deepPopulate(deepSearch)
+                .keyword(options)
+                .page(options, callback);
+
+        
+
+    },
+
+saveInTeam: function (data, callback) {
+        async.waterfall([
+                function (callback) {
+                    Team.findOne().sort({
+                        autoID: -1
+                    }).exec(function (err, team) {
+                        if (err) {
+                            callback(err, null);
+                        } else if (_.isEmpty(team)) {
+                            var year = new Date().getFullYear().toString().substr(2, 2);
+                            var teamid =  "WeDT" + year + 1;
+                            callback(null, teamid);
+                        } else {
+                            var year = new Date().getFullYear().toString().substr(2, 2);
+                            var teamid = "WeDT" + year + ++team.autoID;
+                            callback(null, teamid);
+
+                        }
+                    });
+                },
+                function (teamid, callback) {
+                    data.teamId = teamid;
+                    Team.saveData(data, function (err, teamData) {
+                        if (err) {
+                            console.log("err", err);
+                            callback("There was an error ", null);
+                        } else {
+                            if (_.isEmpty(teamData)) {
+                                callback("No data found", null);
+                            } else {
+                                callback(null, teamData);
+                            }
+                        }
+                    });
+                }
+            ],
+            function (err, data2) {
+                if (err) {
+                    console.log(err);
+                    callback(null, []);
+                } else if (data2) {
+                    if (_.isEmpty(data2)) {
+                        callback(null, []);
+                    } else {
+                        callback(null, data2);
+                    }
+                }
+            });
+    },
+
+    
     };
 
 module.exports = _.assign(module.exports, exports, model);
